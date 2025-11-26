b4b39e6cf7787447de6ffbcfb18424f77ce3d94e
ANDRE PINHEIRO DOS SANTOS <andre.santos.700@ad.ufrn.br>
2025-11-26 09:17:42 -0300

    dadinho

---
+++ dadinho/src/main/java/com/infinity/dadinho/DadinhoApplication.java
@@ -0,0 +1,13 @@
+package com.infinity.dadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadinhoApplication.class, args);
+	}
+
+}
